2c8bef64c24b39ebf40d43f14e734f96a5d0dcf0
Steffen <s.genseleiter@web.de>
2015-11-24 14:49:13 +0000

    first commit

---
+++ src/main/java/de/genseleiter/App.java
@@ -0,0 +1,19 @@
+package de.genseleiter;
+
+/**
+ * Hello world!
+ *
+ */
+import java.*;
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+	Scanner s = new Scanner(System.in);
+	String text = s.nextLine();
+
+        System.out.println(""+ text.toUpperCase());
+    }
+}
